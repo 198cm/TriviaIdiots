3e1edfce15e4700616e5297be554593e54060c83
Noah <noah.van.ommen@hotmail.com>
2019-10-07 11:51:50 +0200

    Client Visuals WPF
    =)

--- TriviaIdiots/TriviaIdiots/MainWindow.xaml.cs
+++ TriviaIdiots/TriviaIdiots/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TriviaIdiots
 {
@@ -20,11 +22,52 @@ namespace TriviaIdiots
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool open = true;
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
         }
 
-      
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JoinButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogoLabel_Loaded(object sender, RoutedEventArgs e)
+        {
+                
+           
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            LogoLabel.Foreground = PickBrush();
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        }
     }
 }
