cfdc0fd0d79609092bda1c3e3e989e59434517f8
Noah <noah.van.ommen@hotmail.com>
2019-10-19 11:41:29 +0200

    oopsie

--- TriviaIdiots/TI-Server/GameHelpCommands.cs
+++ TriviaIdiots/TI-Server/GameHelpCommands.cs
@@ -6,5 +6,18 @@ namespace TI_Server
 {
     class GameHelpCommands
     {
+
+        public string RoomCodeGenerate()
+        {
+            StringBuilder Sbuilder = new StringBuilder();
+            Random r = new Random();
+            char ch;
+            for (int i = 0; i < 4; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * r.NextDouble() + 65)));
+                Sbuilder.Append(ch);
+            } 
+                return Sbuilder.ToString().ToLower();     
+        }
     }
 }
